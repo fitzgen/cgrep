339c04f50593a89884c3b30f93bfb3aae779a9fd
Nick Fitzgerald <fitzgen@gmail.com>
2015-10-30 17:34:22 -0700

    Implement a single-threaded grep.

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+extern crate docopt;
+extern crate rustc_serialize;
+extern crate walker;
+
+use docopt::Docopt;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::io;
+use std::io::BufRead;
+use std::io::Write;
+use std::path;
+use std::process;
+
+const USAGE: &'static str = "
+Usage: cgrep <string> <directory>
+";
+
+#[derive(RustcDecodable)]
+struct Args {
+    arg_string: String,
+    arg_directory: String,
+}
+
+fn get_directory_walker(dir: &path::Path) -> walker::Walker {
+    walker::Walker::new(dir)
+        .unwrap_or_else(|err| {
+            writeln!(io::stderr(), "Error reading directory: {}", &err.description()).unwrap();
+            process::exit(1);
+        })
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.argv(env::args().into_iter()).decode())
+        .unwrap_or_else(|e| e.exit());
+
+    let dir_path = path::Path::new(&args.arg_directory);
+    let entries = get_directory_walker(dir_path);
+
+    let file_paths = entries
+        .filter_map(|entry| entry.map(|e| Some(e))
+                                 .unwrap_or_else(|err| {
+                                     writeln!(io::stderr(), "Error: {}", err.description()).unwrap();
+                                     None
+                                 }))
+        .filter_map(|entry| entry.file_type()
+                                 .map(|file_type| if file_type.is_dir() { None } else { Some(entry) })
+                                 .unwrap_or_else(|err| {
+                                     writeln!(io::stderr(), "Error: {}", err.description()).unwrap();
+                                     None
+                                 }))
+        .map(|entry| entry.path());
+
+    let files = file_paths
+        .map(|path| fs::File::open(path))
+        .filter_map(|path| path.map(|f| Some(f))
+                               .unwrap_or_else(|err| {
+                                   writeln!(io::stderr(), "Error: {}", err.description()).unwrap();
+                                   None
+                               }));
+
+    let readers = files.map(|file| io::BufReader::new(file));
+
+    for reader in readers {
+        for line in reader.lines() {
+            match line {
+                Err(err) => {
+                    writeln!(io::stderr(), "Error: {}", err.description()).unwrap();
+                    continue;
+                },
+                Ok(line) => {
+                    if line.contains(&args.arg_string) {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
+}
